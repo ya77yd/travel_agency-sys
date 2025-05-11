53dffabec33f790d5d6f14bc2c1c1c95091ef634
Nabil Mohamed <nabildalaly@gmail.com>
2025-05-12 00:30:11 +0300

    yazeed

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('login');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
