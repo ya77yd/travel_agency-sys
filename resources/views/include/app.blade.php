58b7f9b87dc057ccede08b06d55ac7374a26d54c
Nabil Mohamed <nabildalaly@gmail.com>
2025-05-11 05:44:46 +0300

    'yazeed'

--- resources/views/include/app.blade.php
+++ resources/views/include/app.blade.php
@@ -4,10 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>@yield('title')</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
+      <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
     <link rel="stylesheet" href="{{ asset('assets/fontawesome-free/css/all.min.css') }}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css">
@@ -22,6 +24,8 @@
     {{-- <link rel="stylesheet" href="{{asset('assets/icheck-bootstrap/icheck-bootstrap.min.css')}}"> --}}
     <!-- JQVMap -->
     <!-- Theme style -->
+        <link rel="stylesheet" href="{{ asset('datatables-bs4/css/dataTables.bootstrap4.css') }}">
+
     <link rel="stylesheet" href="{{ asset('assets/css/adminlte.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
     <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css">
@@ -62,6 +66,9 @@
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
+            <script src="{{ asset('assets/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+
+        <script src="{{ asset('assets/datatables-bs4/jquery.dataTables.js') }}"></script>
     <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- ChartJS -->
     {{-- <script src="plugins/chart.js/Chart.min.js"></script> --}}
@@ -150,7 +157,23 @@
             $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
           });
         })
+
+
+        
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
+
       </script>
+      
 </body>
 
 </html>
