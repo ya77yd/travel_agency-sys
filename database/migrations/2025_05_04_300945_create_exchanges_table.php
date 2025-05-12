edf4da230027ce6f3f2089452f6ac91c9e491442
Nabil Mohamed <nabildalaly@gmail.com>
2025-05-12 23:28:57 +0300

    yazeed

--- database/migrations/2025_05_04_300945_create_exchanges_table.php
+++ database/migrations/2025_05_04_300945_create_exchanges_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('exchanges', function (Blueprint $table) {
             $table->id();
+            $table->decimal('amount', 18, 2);  // المبلغ المدفوع
+            $table->decimal('value', 18, 2);  // المبلغ المدفوع
+
+            $table->decimal('exchange_rate', 18, 2);  // سعر الصرف
+        
+            $table->unsignedBigInteger('amount_currency');  // العملة المستخدمة في الدفع
+
+            $table->unsignedBigInteger('value_currency');  // العملة المستخدمة في الدفع
+            $table->unsignedBigInteger('account'); // الحساب المرتبط بالدفع
+            $table->string('details')->nullable();  // تفاصيل الدفع (اختياري)
+            $table->string('type');  // طريقة الدفع (نقدي، شيك، تحويل بنكي، إلخ)
+            $table->date('date');
+            $table->unsignedBigInteger('created_by');  // المستخدم الذي أنشأ السجل
+            $table->unsignedBigInteger('updated_by')->nullable();  // المستخدم الذي قام بالتحديث
             $table->timestamps();
         });
     }
