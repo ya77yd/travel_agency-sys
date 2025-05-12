81084ea55d4805a42f74dd5eb665ccca87a39fee
Nabil Mohamed <nabildalaly@gmail.com>
2025-05-12 23:31:12 +0300

    yazeed

--- app/Models/Exchanges.php
+++ app/Models/Exchanges.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Exchanges extends Model
 {
-    
+    protected $table = 'exchanges';
+    protected $fillable = [
+        'amount',
+        'value',
+        'exchange_rate',
+        'amount_currency',
+        'value_currency',
+        'account',
+        'details',
+        'type',
+        'date',
+        'created_by',
+        'updated_by'
+    ];
 }
